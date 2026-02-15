ec3034e07cf60028a2e9127be58f82233227a697
StevenMODIMO <stevepetes26@gmail.com>
2026-02-15 13:18:12 +0300

    ai assisted recipe generator

--- next.config.ts
+++ next.config.ts
@@ -6,10 +6,15 @@ const nextConfig: NextConfig = {
       {
         protocol: "https",
         hostname: "t0mrrn6uzcxdc7gp.public.blob.vercel-storage.com",
-        pathname: "/cdn.recipe.users/**"
-      }
-    ]
-  }
+        pathname: "/cdn.recipe.users/**",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+        pathname: "/**",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
